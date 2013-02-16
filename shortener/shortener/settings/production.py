70b353d787893e3b472b5698dc09da0245d036ac
Daniel Greenfeld <pydanny@gmail.com>
2013-02-16 07:59:13 -0800

    Fixing tests to work with test discovery which means close #1

--- shortener/shortener/settings/production.py
+++ shortener/shortener/settings/production.py
@@ -59,5 +59,5 @@ CACHES = {}
 
 ########## SECRET CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-# SECRET_KEY = get_env_setting('SECRET_KEY')
+SECRET_KEY = get_env_setting('SECRET_KEY')
 ########## END SECRET CONFIGURATION
--- shortener/shortener/settings/test.py
+++ shortener/shortener/settings/test.py
@@ -5,7 +5,7 @@ from base import *
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
 TEST_DISCOVER_ROOT = SITE_ROOT
-TEST_DISCOVER_PATTERN = "*"
+TEST_DISCOVER_PATTERN = "*_tests.py"
 ########## IN-MEMORY TEST DATABASE
 DATABASES = {
     "default": {
