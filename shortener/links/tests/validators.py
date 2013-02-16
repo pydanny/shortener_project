0f75bd82bb79f6ced74b9cbc1bb976a6c6337344
Daniel Greenfeld <pydanny@gmail.com>
2013-02-15 17:16:22 -0800

    Working on validator test now

---
+++ shortener/links/tests/validators.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from links.validators import validate_five_characters
+
+
+class ValidatorTests(TestCase):
+
+    def test_validate_five_characters(self):
+        """ TODO write test here """
+        pass
