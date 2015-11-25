e9876859663c2729db3728974d68ab686b53601f
Daniel Roy Greenfeld <pydanny@gmail.com>
2015-11-25 08:03:53 -0800

    Trying a new static URL

--- shortener/core/context_processors.py
+++ shortener/core/context_processors.py
@@ -3,4 +3,5 @@ from django.contrib.sites.models import Site
 def site(request):
     context = {}
     context['site'] = Site.objects.get_current()
+    context['STATIC_URL'] = 'https://s3.amazonaws.com/shortener/'
     return context
